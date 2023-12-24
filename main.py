31e999c357afc037d9a1247ea3a2075dacf6a021
Abdulaziz Ali <variant.abdulaziz@gmail.com>
2023-12-24 07:38:40 +0300

    add demo

---
+++ main.py
@@ -0,0 +1,15 @@
+import xml.etree.ElementTree as ET
+
+# Create the root element
+root = ET.Element("rss")
+
+# Create a child element
+item = ET.SubElement(root, "item")
+title = ET.SubElement(item, "title")
+title.text = "Title"
+
+# Create the XML tree
+tree = ET.ElementTree(root)
+
+# Write to the file
+tree.write("feed.xml")
